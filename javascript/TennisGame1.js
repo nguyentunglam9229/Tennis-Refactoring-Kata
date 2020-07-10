a388b748022406cfc77f4029162b316eb5d46acb
lam <nguyentunglam9229@gmail.com>
2020-07-10 10:56:50 +0700

    decompose conditional

--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -1,4 +1,7 @@
-var TennisGame1 = function(player1Name, player2Name) {
+let scoreMap = [
+    'Love', 'Fifteen', 'Thirty', 'Forty'
+]
+let TennisGame1 = function(player1Name, player2Name) {
     this.m_score1 = 0;
     this.m_score2 = 0;
     this.player1Name = player1Name;
@@ -6,61 +9,54 @@ var TennisGame1 = function(player1Name, player2Name) {
 };
 
 TennisGame1.prototype.wonPoint = function(playerName) {
-    if (playerName === "player1")
+    if (playerName === this.player1Name)
         this.m_score1 += 1;
     else
         this.m_score2 += 1;
 };
 
+
 TennisGame1.prototype.getScore = function() {
-    var score = "";
-    var tempScore = 0;
-    if (this.m_score1 === this.m_score2) {
-        switch (this.m_score1) {
-            case 0:
-                score = "Love-All";
-                break;
-            case 1:
-                score = "Fifteen-All";
-                break;
-            case 2:
-                score = "Thirty-All";
-                break;
-            default:
-                score = "Deuce";
-                break;
-        }
+    let score = "";
+    if (this.isBalance()) {
+        score = this.getBalanceScore();
+
     } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
-        var minusResult = this.m_score1 - this.m_score2;
-        if (minusResult === 1) score = "Advantage player1";
-        else if (minusResult === -1) score = "Advantage player2";
-        else if (minusResult >= 2) score = "Win for player1";
-        else score = "Win for player2";
+        score = this.getTieScore()
     } else {
-        for (var i = 1; i < 3; i++) {
-            if (i === 1) tempScore = this.m_score1;
-            else {
-                score += "-";
-                tempScore = this.m_score2;
-            }
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
-        }
+        score = this.getRegularScore();
     }
     return score;
+
 };
+
+TennisGame1.prototype.isBalance = function() {
+    return this.m_score1 === this.m_score2;
+}
+
+TennisGame1.prototype.getBalanceScore = function () {
+    let score = '';
+    if(this.m_score1 >= 3) {
+        score = 'Deuce'
+    } else {
+        score = scoreMap[this.m_score1] + '-All';
+    }
+    return score;
+}
+
+TennisGame1.prototype.getRegularScore = function () {
+    return scoreMap[this.m_score1] + '-' + scoreMap[this.m_score2];
+}
+
+TennisGame1.prototype.getTieScore = function () {
+    let score = '';
+    let minusResult = this.m_score1 - this.m_score2;
+    if (minusResult === 1) score = `Advantage ${this.player1Name}`;
+    else if (minusResult === -1) score = `Advantage ${this.player2Name}`;
+    else if (minusResult >= 2) score = `Win for ${this.player1Name}`;
+    else score = `Win for ${this.player2Name}`;
+    return score;
+}
 
 if (typeof window === "undefined") {
     module.exports = TennisGame1;
